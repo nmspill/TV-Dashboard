1c1ba531f5e5596ae2607c75e4fae849cb85be8e
nolan <nolanspill@gmail.com>
2020-06-10 15:38:46 -0400

    Added correct current weather icons, removed unsed css document

--- Dashboard/Index.js
+++ Dashboard/Index.js
@@ -1,24 +1,27 @@
-function startTime() {
+//sets the current time
+function setCurrentTime() {
   let today = new Date();
   let h = today.getHours();
   let m = today.getMinutes();
   let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
+  m = formatMinAndSec(m);
+  s = formatMinAndSec(s);
   h = ((h + 11) % 12) + 1;
   document.getElementById("clock").innerHTML = h + ":" + m;
   document.getElementById("clock-seconds").innerHTML = s;
-  setTimeout(startTime);
+  setTimeout(setCurrentTime);
 }
 
-function checkTime(i) {
+//adds a 0 to the end of the MM and SS place if needed
+function formatMinAndSec(i) {
   if (i < 10) {
     i = "0" + i;
   }
   return i;
 }
 
-function startDate() {
+//sets the current date
+function setCurrentDate() {
   let today = new Date();
   let day = today.getDay();
   let dayOfMonth = today.getDate();
@@ -41,7 +44,7 @@ function startDate() {
     "Sunday",
     "Monday",
     "Tuesday",
-    "Wedday",
+    "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
@@ -52,6 +55,7 @@ function startDate() {
   document.getElementById("month").innerHTML = months[theMonth] + " ";
 }
 
+//formats the day of the month to have the correct ending
 function formatDate(i) {
   var j = i % 10,
     k = i % 100;
@@ -67,10 +71,15 @@ function formatDate(i) {
   return i + "th";
 }
 
-function getDayWeatherData(city) {
+//sets the daily weather data
+function setDailyWeatherData(city, lat, lon) {
   const key = "d2271c51ac924c325f42b6450e49d991";
   const URL =
-    "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=41.0148&lon=-74.0120&&exclude=hourly,minutely&appid=" +
+    "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&&exclude=hourly,minutely&appid=" +
     key;
   fetch(URL)
     .then(function (resp) {
@@ -94,10 +103,15 @@ function getDayWeatherData(city) {
     });
 }
 
-function setCurrentTemp() {
+//sets the current weather data
+function setCurrentWeatherData(lat, lon) {
   const key = "d2271c51ac924c325f42b6450e49d991";
   const URL =
-    "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=41.0148&lon=-74.0120&&exclude=hourly,minutely,daily&appid=" +
+    "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&&exclude=hourly,minutely&appid=" +
     key;
   fetch(URL)
     .then(function (resp) {
@@ -106,13 +120,61 @@ function setCurrentTemp() {
     .then(function (data) {
       console.log(data);
       let temp = Math.round(data["current"]["temp"]);
+      let currentIcon = data["current"]["weather"][0]["main"];
+      let sunsetTime = data["current"]["sunset"];
       document.getElementById("temp").innerHTML = temp + "°";
+      console.log(currentIcon);
+      formatCurrentIcon(currentIcon, sunsetTime);
     })
     .catch(function () {
       console.log("error");
     });
 }
 
+//formats current weather icon
+function formatCurrentIcon(s, sunsetTime) {
+  let currentTime = new Date().getTime();
+  switch (s) {
+    case "Thunderstorm":
+      document.getElementById("currentIcon").className = "wi wi-thunderstorm";
+      break;
+
+    case "Drizzle":
+      document.getElementById("currentIcon").className = "wi wi-showers";
+      break;
+
+    case "Rain":
+      document.getElementById("currentIcon").className = "wi wi-rain";
+      break;
+
+    case "Snow":
+      document.getElementById("currentIcon").className = "wi wi-snow";
+      break;
+
+    case "Clear":
+      document.getElementById("weatherIcon").className =
+        "weather-icon-more-top";
+      if (currentTime > sunsetTime) {
+        document.getElementById("currentIcon").className = "wi wi-day-sunny";
+      } else {
+        document.getElementById("currentIcon").className = "wi wi-night-clear";
+      }
+      break;
+
+    case "Clouds":
+      document.getElementById("weatherIcon").className =
+        "weather-icon-more-top";
+      document.getElementById("currentIcon").className = "wi wi-cloudy";
+      break;
+
+    default:
+      document.getElementById("weatherIcon").className =
+        "weather-icon-more-top";
+      document.getElementById("currentIcon").className = "wi wi-dust";
+  }
+}
+
+//formats the time from UNIX to HH:MM
 function formatTime(unix_timestamp) {
   let date = new Date(unix_timestamp * 1000);
   let hours = date.getHours();
@@ -121,13 +183,17 @@ function formatTime(unix_timestamp) {
   return hours + ":" + minutes.substr(-2);
 }
 
+//sets current weather data every 30 minutes
 window.setInterval(function () {
-  this.setCurrentTemp();
+  this.setCurrentWeatherData();
 }, 60000 * 30);
 
+//is called when the window is loaded or refreshed
 window.onload = function () {
-  this.startTime();
-  this.startDate();
-  this.setCurrentTemp();
-  this.getDayWeatherData("River Vale");
+  const lat = "41.0148";
+  const lon = "-74.0120";
+  this.setCurrentTime();
+  this.setCurrentDate();
+  this.setCurrentWeatherData(lat, lon);
+  this.setDailyWeatherData("River Vale", lat, lon);
 };
